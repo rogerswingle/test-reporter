61867f49a9b80f4eaa771b2b8002229a5e2a9064
rogerswingle <rogerswingle@gmail.com>
2024-08-28 08:44:52 -0400

    still need the double click

--- src/main.ts
+++ src/main.ts
@@ -166,7 +166,7 @@ class TestReporter {
 
     const {listSuites, listTests, onlySummary, useActionsSummary, badgeTitle} = this
 
-    let baseUrl = github.context.serverUrl + '/' + github.context.repo.owner + '/' + github.context.repo.repo + '/actions/runs/' + github.context.runId
+    let baseUrl = ''
     if (this.useActionsSummary) {
       const summary = getReport(results, {listSuites, listTests, baseUrl, onlySummary, useActionsSummary, badgeTitle})
 
